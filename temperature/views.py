75cc2c03c40940b62c926d00a07a536e027d0d40
Cristian D. Ceballos <cdceballor@eafit.edu.co>
2020-05-27 16:17:26 -0500

    bdw

---
+++ temperature/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, HttpResponse
+import requests
+
+def temperature(request):
+    # Verifica si hay un parámetro value en la petición GET
+    if 'value' in request.GET:
+        value = request.GET['value']
+        longi = request.GET['longi']
+        lati = request.GET['lati']
+        tipTerr = request.GET['tipTerr']
+        # Verifica si el value no esta vacio
+        if value:
+            # Crea el json para realizar la petición POST al Web Service
+            args = {'value': value, 'lati': lati, 'longi':longi, 'tipTerr': tipTerr, 'type': 'temperature'}
+            #response = requests.post('http://127.0.0.1:8000/temperaturas/', args)
+            response = requests.post('https://pi1-eafit-cdceballor.azurewebsites.net/temperaturas/', args)
+            # Convierte la respuesta en JSON
+            temperature_json = response.json()
+
+    # Realiza una petición GET al Web Services
+    #response = requests.get('http://127.0.0.1:8000/temperaturas/')
+    response = requests.get('https://pi1-eafit-cdceballor.azurewebsites.net/temperaturas/')
+    # Convierte la respuesta en JSON
+    temperatures = response.json()
+    # Rederiza la respuesta en el template measure
+    return render(request, "temperature/temperature.html", {'temperatures': temperatures})
